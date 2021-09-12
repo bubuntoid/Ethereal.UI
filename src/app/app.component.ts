d87c7c9e2f0c0f8a294441265c7a31f76c3383f8
Dmitrii Stafeev <bubuntoid@gmail.com>
2021-09-12 20:14:23 +0300

    save changess

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PreviewService } from './services/preview.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(private previewService : PreviewService) { }
+
+  isLoading : boolean = false;
+
   url: string = "";
   onUrlChanged(event: any){
     this.url = event;
+    this.previewService.getDescriptionPreview(this.url).subscribe(response => {
+      this.description = response;
+    });
   }
 
-  customDescription: boolean = false;
-  onCustomDescriptionChanged(event: any){
-    this.customDescription = event;
+  useCustomDescription: boolean = false;
+  onUseCustomDescriptionChanged(event: any){
+    this.useCustomDescription = event;
   }
+
+  description: string = "";
 }
